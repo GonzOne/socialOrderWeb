47ba2db6a0f4a35a2b86e70f1d5d8a7442e3a8d4
Manuel Gonzale <design@stheory.com>
2016-03-10 12:25:40 -0800

    added angualr-loading-bar

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary']);
+    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary', 'angular-loading-bar']);
 
 })();
