c0bf6293fedb97c3363a72f78ce7d0149c6adfb4
Manuel Gonzale <design@stheory.com>
2016-05-31 15:45:33 -0700

    added scroll-spy

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,8 @@
   'use strict';
 
   angular
-    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary', 'angular-loading-bar']);
+    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary', 'angular-loading-bar', 'duScroll'])
+    .value('duScrollDuration', 1000)
+    .value('duScrollOffset', 80);
 
 })();
