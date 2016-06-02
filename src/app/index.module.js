67a5751802d8b5862c395b987d96fc06859c2d20
Manuel Gonzale <design@stheory.com>
2016-06-01 19:11:47 -0700

    added ngPassword

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary', 'angular-loading-bar', 'duScroll'])
+    .module('socialOrderWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ui.grid', 'toastr','LocalStorageModule', 'firebase', 'blockUI', 'toggle-switch', 'ngFileUpload', 'angular.backtop', 'ct.ui.router.extras','google.places', 'cloudinary', 'angular-loading-bar', 'duScroll', 'ngPassword'])
     .value('duScrollDuration', 1000)
     .value('duScrollOffset', 80);
 
