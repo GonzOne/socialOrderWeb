2b630d96f08715a8dcb3c8df51600f045bdc9b9e
Manuel Gonzale <design@stheory.com>
2016-06-01 19:28:48 -0700

    staff stuff that wasnt checked in - needs to be tested

--- src/app/venue/staff/venueStaff.controller.js
+++ src/app/venue/staff/venueStaff.controller.js
@@ -8,85 +8,14 @@
     /** @ngInject */
     function VenueStaffController(admin, staff, appGlobalVars, dataTemplates, venueService, profileService, Auth, blockUI, uiGridConstants, $log) {
         var vm = this;
-        vm.isAdminModCollapsed = false;
-        vm.isStaffModCollapsed = false;
-        vm.venueAdminColumnDef = [
-            { field: 'id', name: '', cellTemplate: 'app/admin/partials/buttons/edit-venue-detail-admin-button.html', width: 34 },
-            { name: 'First', field: 'firstName', minWidth: 150, enableCellEdit: false },
-            { name: 'Last', field: 'lastName', minWidth: 150, enableCellEdit: false  },
-            { name: 'Email', field: 'email', minWidth: 150, enableCellEdit: false  }
-        ];
-
-        vm.venueStaffColumnDef = [
-            { field: 'id', name: '', cellTemplate: 'app/admin/partials/buttons/edit-venue-detail-staff-button.html', width: 34 },
-            { name: 'First',field: 'firstName', minWidth: 150, enableCellEdit: false },
-            { name: 'Last', field: 'lastName', minWidth: 150, enableCellEdit: false  },
-            { name: 'Email', field: 'email', minWidth: 150, enableCellEdit: false  }
-        ];
+        vm.venueId = appGlobalVars.getVenueId();//need to refactor this out!
         //export
-        vm.toggleModule = toggleModule;
         vm.updateGrid = updateGrid;
 
         (function initController() {
             createAdminList(admin);
             createStaffList(staff);
         })();
-
-        function toggleModule (module) {
-           switch(module) {
-               case 'admin':
-                   vm.isAdminModCollapsed = !vm.isAdminModCollapsed;
-                   break;
-               case 'staff':
-                   vm.isStaffModCollapsed = !vm.isStaffModCollapsed;
-                   break;
-               default:
-           }
-        }
-        /*
-        function getVenue (){
-            var vKey =  appGlobalVars.getVenueId();
-            $log.log('VenueDetailController - getVenue : vKey ',vKey);
-            venueService.getVenueById(vKey).then(function (data) {
-                $log.log('getVenue - Retrieved venue :', data);
-                vm.venue = data;
-
-                if (typeof data.admin != 'undefined'){
-                    $log.log('admin ',data.admin);
-                    venueService.getVenueAdminListById(vKey).then(function (data) {
-                        $log.log('admin list', data)
-                        createAdminList(data)
-                    }, function (error) {
-                        $log.log('Error:', error);
-                    })
-
-                }
-                $log.log('has menu ',data.menu_id )
-                //display edit or add button for menu
-                if (typeof data.menu_id  != 'undefined'){
-                    vm.menuActive = true;
-
-                }else {
-                    vm.menuActive = false;
-                }
-
-                //display staff
-                if (typeof data.staff != 'undefined'){
-                    $log.log('staff ',data.staff);
-                    venueService.getVenueStaffListById(vKey).then(function (data) {
-                        $log.log('staff list', data)
-                        createStaffList(data)
-                    }, function (error) {
-                        $log.log('Error:', error);
-                    })
-
-                }
-
-            }, function (error) {
-                $log.log('Error:', error);
-            })
-        }
-        */
         function createStaffList(inArray) {
             $log.log('createStaffList in Array ', inArray)
             vm.venueStaff = [];
@@ -126,7 +55,7 @@
         }
 
         function updateGrid (val) {
-            $log.log('venueDetailController : updateGrid ', val)
+            $log.log('VenueStaffController : updateGrid ', val)
             var vKey =  appGlobalVars.getVenueId();
             switch (val) {
                 case 'venue_admin':
